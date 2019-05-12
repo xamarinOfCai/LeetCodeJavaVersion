c1fa613c7786b15c7566317c3b18d09744946408
caijingjin <caijj1@chanjet.com>
2019-05-12 13:47:35 +0800

    第二道题AddTwoNumbers的答案

---
+++ src/addtwonumbers/AddTwoNumbers.java
@@ -0,0 +1,42 @@
+package addtwonumbers;
+
+/**
+ * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order
+ * and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ *
+ * Example:
+ *
+ * Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * Output: 7 -> 0 -> 8
+ * Explanation: 342 + 465 = 807.
+ * @ClassName AddTwoNumbers
+ * @Description TODO
+ * @Author cjj
+ * @Date 2019-05-12 13:44
+ * @Version 1.0
+ */
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+
+    }
+
+}
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode listNode = new ListNode(0);
+
+        return listNode;
+    }
+}
---
+++ src/addtwonumbers/ListNode.java
@@ -0,0 +1,17 @@
+package addtwonumbers;
+
+/**
+ * @ClassName ListNode
+ * @Description TODO
+ * @Author cjj
+ * @Date 2019-05-12 13:45
+ * @Version 1.0
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
