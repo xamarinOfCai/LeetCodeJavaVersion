5dd8c69764348405b218c5fae8c989fe3b8d9df0
caijingjin <caijj1@chanjet.com>
2019-06-09 16:28:48 +0800

    第二道题AddTwoNumbers的答案

---
+++ src/LongestSubStringWithoutRepeatingCharacters0003.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName LongestSubStringWithoutRepeatingCharacters0003
+ * @Description 第三道题
+ * @Author cjj
+ * @Date 2019-06-09 15:24
+ * @Version 1.0
+ */
+public class LongestSubStringWithoutRepeatingCharacters0003 {
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        List<String> testCases = new ArrayList<String>();
+
+        testCases.add("abcabcbb");
+        testCases.add("bbbbb");
+        testCases.add("pwwkew");
+        testCases.add("au");
+
+        for(int h = 0; h < testCases.size(); h++) {
+            int length = s.lengthOfLongestSubString(testCases.get(h));
+            System.out.println("最长的字符串的长度为 = [" + length + "]");
+        }
+    }
+
+}
+
+class Solution{
+    /**
+     * 计算不包含无重复子串的长度
+     * 思路：逐个检查所有的子字符串，看它是否不含有重复的字符
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubString(String s){
+        int length = 1;
+        if(s == null || s.length()== 0 ){
+            return 0;
+        }
+        if(s.length() == 1){
+            return length;
+        }
+
+        String maxString = "";
+        for(int i = 0; i < s.length(); i++){
+            String sub = String.valueOf(s.charAt(i));
+            for(int j = i+1; j < s.length(); j++){
+                char tempChar = s.charAt(j);
+                if(sub.indexOf(tempChar) != -1){
+                    //每次跳出循环时候，需要比较当前
+                    // 最大的字符串与目前得到的字符串的长度
+                    if(maxString.length() < sub.length()){
+                        maxString = sub;
+                    }
+                    break;
+                }else{
+                    sub += tempChar;
+                }
+                //每次跳出循环时候，需要比较当前
+                // 最大的字符串与目前得到的字符串的长度
+                if(maxString.length() < sub.length()){
+                    maxString = sub;
+                }
+            }
+
+        }
+        length = maxString.length();
+        System.out.println("maxString = [" + maxString + "]");
+        return length;
+    }
+
+}
--- src/addtwonumbers/AddTwoNumbers.java
+++ src/addtwonumbers/AddTwoNumbers.java
@@ -1,5 +1,7 @@
 package addtwonumbers;
 
+import java.util.concurrent.ExecutorService;
+
 /**
  * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order
  * and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
@@ -35,8 +37,90 @@ public class AddTwoNumbers {
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode listNode = new ListNode(0);
+//        ListNode listNode = new ListNode(0);
+//        int length1 = getLength(l1);
+//        int length2 = getLength(l2);
+//        if(length1 == 0){
+//            return l2;
+//        }
+//        if(length2 == 0){
+//            return l1;
+//        }
+//        int minLength = length1 > length2 ? length2:length1;
+//        int preSum = 0;
+//        int beginSum = l1.val + l2.val;
+//        if(beginSum >= 10){
+//            beginSum = beginSum - 10;
+//            preSum = 1;
+//        }
+//        ListNode beginNode = new ListNode(beginSum);
+//        ListNode next1 = l1.next;
+//        ListNode next2 = l2.next;
+//        ListNode next = new ListNode(0);
+//        for(int i = 1; i < minLength; i++){
+//            if(next1 != null && next2 != null){
+//                int currentSum = next1.val+next2.val+preSum;
+//                if(currentSum >= 10){
+//                    currentSum = currentSum -10;
+//                    preSum = 1;
+//
+//                }
+//            }
+//        }
+//
+//
+//        return listNode;
 
-        return listNode;
+        int preSum = 0;
+        int sum = 0;
+        ListNode head = new ListNode(0);
+        ListNode pre = head;
+        //先直接计算两者，直到最短的退出
+        while(l1 != null && l2 != null ){
+            sum = l1.val+l2.val;
+            sum += preSum;
+            preSum = sum / 10;
+            sum = sum % 10;
+            ListNode currentNode = new ListNode(sum);
+            head.next = currentNode;
+            head =  currentNode;
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+
+        ListNode restNode = (l1 == null) ? l2:l1;
+        //短的结束后，另外一个继续计算和进位
+        while(restNode != null){
+            sum = restNode.val;
+            sum += preSum;
+            preSum = sum / 10;
+            sum = sum % 10;
+            ListNode temp = new ListNode(sum);
+            head.next = temp;
+            head = temp;
+            restNode = restNode.next;
+        }
+        //最后还剩下进位的
+        if(preSum > 0){
+            ListNode lastNode = new ListNode(preSum);
+            head.next = lastNode;
+            head = lastNode;
+        }
+        return pre.next;
+    }
+
+    private int getLength(ListNode listNode){
+        int i = 0;
+        if(listNode == null){
+            return i;
+        }else{
+            i = 1;
+        }
+        ListNode next = listNode.next;
+        while(next != null){
+            i++;
+            next = next.next;
+        }
+        return i;
     }
 }
